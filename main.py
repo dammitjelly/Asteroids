f4f042d45efea5383fa6b3e95f0d759bcd831c0c
dammitjelly <dammitjelly@gmail.com>
2025-04-21 16:00:31 +1000

    added player sprite and rotation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -8,13 +9,23 @@ def main():
         f"Screen width: {SCREEN_WIDTH}\n"
         f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        player.update(dt)
+        player.draw(screen)
+        
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
